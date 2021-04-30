e0a00b53bb22ec16cfb0237c9c27bd7ffc9ac9fc
Xing Li <ilgnix.0211@icloud.com>
2021-04-30 01:41:07 +0000

    Add websocket

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/xinglixing/chat-app/internal/handlers"
 )
 
 func main() {
 	mux := routes()
 
-	// log.Println("Start channel listener...")
-	// handlers.ListenToWSChannel()
+	log.Println("Start channel listener...")
+	go handlers.ListenToWSChannel()
 
 	log.Println("Starting web server on port 8080")
 	_ = http.ListenAndServe(":8080", mux)
